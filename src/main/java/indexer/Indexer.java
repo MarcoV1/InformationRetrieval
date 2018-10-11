2673d928fb2ceaea831de634798d32afc90136e3
Dimitri <dimitrisilva@ua.pt>
2018-10-11 16:21:37 +0100

    Basic Indexer

--- src/main/java/indexer/Indexer.java
+++ src/main/java/indexer/Indexer.java
@@ -5,15 +5,32 @@
  */
 package indexer;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javafx.util.Pair;
+
 public class Indexer {
 
     public Indexer() {
         
         
     }
-    
-    
-    
-    
-    
+
+    public Map<String, Map<Integer, Integer>> index(List<Pair<String, Integer>> termos) {
+        Map<String, Map<Integer, Integer>> index = new HashMap<>();
+        for (Pair<String, Integer> x : termos) {
+            if (!index.containsKey(x.getKey())) {
+                index.put(x.getKey(), new HashMap<>());
+            }
+            if (!index.get(x.getKey()).containsKey(x.getValue())) {
+                index.get(x.getKey()).put(x.getValue(), 1);
+            } else {
+                int cur = index.get(x.getKey()).get(x.getValue());
+                index.get(x.getKey()).put(x.getValue(), cur+1);
+            }
+        }
+        return index;
+    }
+     
 }
