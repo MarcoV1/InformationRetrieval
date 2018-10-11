9a1d73d0b8b814e2434e178494cc2b787b70aae9
MarcoV1 <marcoandreventura@ua.pt>
2018-10-11 14:49:28 +0100

    indexer added

---
+++ src/main/java/indexer/IndexReader.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package indexer;
+
+import java.io.File;
+
+/**
+ *
+ * @author marco
+ */
+public class IndexReader {
+
+    public IndexReader() {
+    }
+    
+    
+    
+    public void readFile(File f) {
+        
+        
+        
+    }
+    
+    
+}
