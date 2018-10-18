e13a0fd31ac10588ea1a6b9e924354c2de0e3fac
Dimitri <dimitrisilva@ua.pt>
2018-10-18 19:57:44 +0100

    Deixa de funcionar corretamente depois de gravar o primeiro bloco

--- src/main/java/indexer/Indexer.java
+++ src/main/java/indexer/Indexer.java
@@ -5,16 +5,31 @@
  */
 package indexer;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
 import javafx.util.Pair;
 
 public class Indexer {
 
+    private Map<String, Map<Integer, Integer>> index;
+    private int currentBlock, inmem;
+
     public Indexer() {
-        
-        
+        index = new HashMap<>();
+        currentBlock = 1;
+        inmem = 0;
     }
 
     public Map<String, Map<Integer, Integer>> index(List<Pair<String, Integer>> termos) {
@@ -27,10 +42,50 @@ public class Indexer {
                 index.get(x.getKey()).put(x.getValue(), 1);
             } else {
                 int cur = index.get(x.getKey()).get(x.getValue());
-                index.get(x.getKey()).put(x.getValue(), cur+1);
+                index.get(x.getKey()).put(x.getValue(), cur + 1);
             }
         }
         return index;
     }
-     
+
+    public void saveBlock() {
+        File outputFile = new File("index_block_" + currentBlock+++".txt");
+        SortedSet<String> sorted = new TreeSet<>(index.keySet());
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile))) {
+            for (String termo : sorted) {
+                bw.write(termo);
+                for (Integer i : index.get(termo).keySet()) {
+                    String w = "," + i + ":" + index.get(termo).get(i);
+                    bw.write(w);
+                }
+                bw.newLine();
+                
+            }
+            bw.close();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+        index = null;
+        index = new HashMap<>();
+    }
+
+    // Math.pow(1024, 8)
+    // 4gb 4294967296
+    public void addToSPIMIIndex(List<Pair<String, Integer>> list) {
+        //System.out.println(Runtime.getRuntime().freeMemory()+ " "+ 1024*256);
+        for (Pair<String, Integer> x : list) {
+            if (!index.containsKey(x.getKey())) {
+                index.put(x.getKey(), new HashMap<>());
+            }
+            if (!index.get(x.getKey()).containsKey(x.getValue())) {
+                index.get(x.getKey()).put(x.getValue(), 1);
+            } else {
+                int cur = index.get(x.getKey()).get(x.getValue());
+                index.get(x.getKey()).put(x.getValue(), cur + 1);
+            }
+        }
+
+    }
 }
