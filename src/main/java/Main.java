00647ef182ce0e07322d562613c3611e9734999c
Dimitri <dimitrisilva@ua.pt>
2018-10-23 22:55:22 +0100

    Revert "Max memory ajusts"
    This reverts commit ba2138e36d3662940d1f7edb48a06b3614b7bb54.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,19 +17,11 @@ public class Main {
     public static void main(String[] args) {
 
         CorpusReader corpus = null;
-        int maxMem = 0;
         try {
             // src\main\java\text\amazon_reviews_us_Wireless_v1_00.tsv -xmx 256m
             corpus = new TSVReader(args[0]);
-
-        } catch (Exception ex) {
-            System.out.println("Usage: target_file [max_memory(MB)]");
-            System.exit(-1);
-        }
-        try {
-            maxMem = Integer.parseInt(args[1]);
-        } catch (Exception ex) {
-            System.out.println("Usage: target_file [max_memory(MB)] " + args[1]);
+        } catch (IOException ex) {
+            System.out.println("No file :(");
         }
         Tokenizer tokenizer = null;
         Indexer indexer = new Indexer();
@@ -43,28 +35,32 @@ public class Main {
 
         if (typeTokenizer.equals("simples")) {
             tokenizer = new SimpleTokenizer();
-        } else if (typeTokenizer.equals("improved")) {
+        }
+        else if (typeTokenizer.equals("improved")) {
             System.out.println("Com ou sem stemmer? (c/s)");
-            do {
+             do{
                 typeTokenizer = sc.nextLine().toLowerCase();
-
-            } while (!typeTokenizer.equals("c") && !typeTokenizer.equals("s"));
-
+            
+            }while(!typeTokenizer.equals("c") && !typeTokenizer.equals("s"));
+     
             if (typeTokenizer.equals("s")) {
                 tokenizer = new ImprovedTokenizer();
-            } else if (typeTokenizer.equals("c")) {
+            }
+            else if (typeTokenizer.equals("c")) {
                 tokenizer = new ImprovedTokenizer(new englishStemmer());
             }
         }
-
+        Runtime runtime = Runtime.getRuntime();
+        
         Document doc = corpus.nextDocument();
         System.out.println("Starting block by block index");
         while (doc != null) {
-            if (checkMem(maxMem)) {
+            if ((runtime.totalMemory() - runtime.freeMemory()) / 1000000 >= (1024*0.85)) {
+                System.out.println((runtime.totalMemory() - runtime.freeMemory()) / 1000000);
                 System.out.println("Saving Block");
                 indexer.saveBlock();
                 System.gc();
-                //System.out.println((runtime.totalMemory() - runtime.freeMemory()) / 1000000);
+                System.out.println((runtime.totalMemory() - runtime.freeMemory()) / 1000000);
             }
             indexer.addToSPIMIIndex(tokenizer.tokenize(doc), doc.getId());
             doc = corpus.nextDocument();
@@ -76,16 +72,6 @@ public class Main {
             indexer.mergeBlocks(b);
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    private static boolean checkMem(int maxMem) {
-        Runtime runtime = Runtime.getRuntime();
-        //System.out.println(runtime.totalMemory()/ 1000000 + "  " + runtime.freeMemory()/ 1000000);
-        if (maxMem > 0) {
-            return (runtime.totalMemory()/1000000 >= maxMem && (runtime.totalMemory() - runtime.freeMemory()) / 1000000 >= (maxMem * 0.80));
-        } else {
-            return (runtime.freeMemory() / runtime.totalMemory() < 0.20);
         }
     }
 }
